a41a5bdabc9ab937e26929539f052c5ed9321adf
Kakarot2002 <11931@cic.du.ac.in>
2023-04-03 10:04:52 +0530

    Add Hasing

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const app = express();
 
 
-//console.log(process.env.API_KEY);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -26,8 +27,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -45,18 +44,22 @@ app.get("/register", (req, res) =>{
 });
 
 app.post("/register", (req, res) =>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
 
-    newUser.save()
-    .then(()=>{
-        res.render("secrets");
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save()
+        .then(()=>{
+            res.render("secrets");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    });
+    
 
 });
 
@@ -67,9 +70,11 @@ app.post("/login", (req, res) =>{
 
     User.findOne({email:username})
     .then((foundUser)=>{
-        if(foundUser.password === password){
-            res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result === true){
+                res.render("secrets");
+            }
+        });
     })
     .catch((err)=>{
         console.log(err);
